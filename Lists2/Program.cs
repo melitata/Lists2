ef958e420d2a4da461bb761ac022631a8d0222d6
Melisa Kılıç <mk_i3@hotmail.com>
2024-11-04 21:08:23 +0300

    Proje dosyası ekle.

---
+++ Lists2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> coffeeNames = new List<string>();
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Lütfen kahve ismi girin: ");
+            string coffee = Console.ReadLine();
+            coffeeNames.Add(coffee);
+        }
+        Console.WriteLine("\nKahve İsimleri:");
+        foreach (string coffee in coffeeNames)
+        {
+            Console.WriteLine(coffee);
+        }
+    }
+}
